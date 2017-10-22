23cffa6f7b93fee884abc17b683fca6694f8568a
AleksandrKaratchenya <sanya.karatchenya@mail.ru>
2017-10-22 21:08:40 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -4,17 +4,20 @@ class FSM {
      * @param config
      */
     constructor(config) {
-    if(!config)
+        this.count=0;
+        if(!config)
             return Error;
        this.config=config;
-        this.init=this.config.initial;
+       this.init=this.config.initial;
+
     }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() { 
+    getState() {
+        
         return this.config.initial;
     }
 
@@ -23,10 +26,12 @@ class FSM {
      * @param state
      */
     changeState(state) {
-        if(state!='normal'&&state!='busy'&&state!='sleeping'&&state!='hungry')
+      if(state!='normal'&&state!='busy'&&state!='sleeping'&&state!='hungry')
        return Error;
-       this.config.initial=state;
-       
+       this.config.prev=this.config.initial;
+        this.count++;
+       this.config.initial=state; 
+
     }
 
     /**
@@ -34,17 +39,24 @@ class FSM {
      * @param event
      */
     trigger(event) {
-    var a=this.config.initial;
+        //if(this.initial=='normal')
+        var a=this.config.initial;
          if(!this.config.states[a].transitions[event])
             return Error;
+        this.config.prev=a;
+        this.count++;
        this.config.initial= this.config.states[a].transitions[event];
+        //this.config.states.normal.transitions.event;
     }
 
     /**
      * Resets FSM state to initial.
      */
     reset() {
-    this.config.initial=this.init;
+        this.config.prev=this.config.initial;
+        this.count++;
+        this.config.initial=this.init;
+
     }
 
     /**
@@ -54,23 +66,22 @@ class FSM {
      * @returns {Array}
      */
     getStates(event) {
-             var arr=['normal', 'busy', 'hungry', 'sleeping'];
+        var arr=['normal', 'busy', 'hungry', 'sleeping'];
         if(!event)
             return arr;
-        if(event!='get_hungry'&&event!='study'&&event!='get_tired'&&event!='eat'&&event!='get_up')
+        if(event!='get_hungry'&&event!='study'&&event!='eat'&&event!='get_up')
        return [];
    var returns_arr=[];
-   for(var j=0,k;j<4;j++)
+   for(let j=0, k=0;j<4;j++)
    {
     var i=arr[j];
 if(this.config.states[i].transitions[event])
   {
-    returns_arr[k]=this.config.states[i];
+    returns_arr[k]=i;
     k++;
   }
 }
 return returns_arr;
-  
     }
 
     /**
@@ -78,14 +89,24 @@ return returns_arr;
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+     if(this.config.prev&&this.count)
+     {
+        this.count--;
+        this.config.initial=this.config.prev;
+        return true;
+    }
+    return false;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        
+    }
 
     /**
      * Clears transition history
